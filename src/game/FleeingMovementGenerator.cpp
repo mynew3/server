eddf5f22a14701a7b9e5e7ac6507e08ca48e7d6a
fr1nge <v4l33@hotmail.com>
2012-07-16 12:17:13 +0300

    [rev.009] Add new wrappers for Fear effects Based on @802b6bd
    Signed-off-by: fr1nge <v4l33@hotmail.com>

--- src/game/FleeingMovementGenerator.cpp
+++ src/game/FleeingMovementGenerator.cpp
@@ -101,11 +101,23 @@ bool FleeingMovementGenerator<T>::_getPoint(T &owner, float &x, float &y, float 
     float curr_x, curr_y, curr_z;
     owner.GetPosition(curr_x, curr_y, curr_z);
 
-    x = curr_x + dist*cos(angle);
-    y = curr_y + dist*sin(angle);
-    z = curr_z;
-
-    owner.UpdateAllowedPositionZ(x, y, z);
+    WorldLocation destLoc;
+    destLoc.coord_x = curr_x + dist*cos(angle);
+    destLoc.coord_y = curr_y + dist*sin(angle);
+    destLoc.coord_z = curr_z;
+    owner.MovePositionToFirstCollision(destLoc, owner.GetObjectScale(), owner.GetOrientation());
+    if (fabs(owner.GetMap()->GetTerrain()->GetHeight(destLoc.coord_x,destLoc.coord_y,destLoc.coord_z,true) - owner.GetPositionZ()) > 2.0f)
+    {
+        x = curr_x;
+        y = curr_y;
+        z = curr_z;
+    }
+    else
+    {
+        x = destLoc.coord_x;
+        y = destLoc.coord_y;
+        z = destLoc.coord_z + owner.GetObjectScale();
+    }
 
     return true;
 }
