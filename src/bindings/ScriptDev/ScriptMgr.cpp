5b273d410ab951e0fea1d42d3f7be1ac116cee18
Carl Hjerpe <LilleCarl@Gmail.com>
2012-07-19 21:39:06 +0200

    Add scripting system back.

---
+++ src/bindings/ScriptDev/ScriptMgr.cpp
@@ -0,0 +1,382 @@
+/* Copyright (C) 2006 - 2012 ScriptDev2 <http://www.scriptdev2.com/>
+ * This program is free software licensed under GPL version 2
+ * Please see the included DOCS/LICENSE.TXT for more information */
+
+#include "precompiled.h"
+#include "Config/Config.h"
+#include "config.h"
+#include "Database/DatabaseEnv.h"
+#include "DBCStores.h"
+#include "ObjectMgr.h"
+#include "ProgressBar.h"
+#include "../system/ScriptLoader.h"
+#include "../../../game/ScriptMgr.h"
+
+typedef std::vector<Script*> ScriptVec;
+int num_sc_scripts;
+ScriptVec m_scripts;
+
+void FillSpellSummary();
+
+struct TSpellSummary {
+    uint8 Targets;                                          // set of enum SelectTarget
+    uint8 Effects;                                          // set of enum SelectEffect
+}extern *SpellSummary;
+
+MANGOS_DLL_EXPORT
+void FreeScriptLibrary()
+{
+    // Free Spell Summary
+    delete []SpellSummary;
+
+    // Free resources before library unload
+    for (ScriptVec::const_iterator itr = m_scripts.begin(); itr != m_scripts.end(); ++itr)
+        delete *itr;
+
+    m_scripts.clear();
+
+    num_sc_scripts = 0;
+}
+
+MANGOS_DLL_EXPORT
+void InitScriptLibrary()
+{
+    outstring_log("");
+
+    outstring_log("SD2: Loading C++ scripts");
+    BarGoLink bar(1);
+    bar.step();
+    outstring_log("");
+
+    // Resize script ids to needed ammount of assigned ScriptNames (from core)
+    m_scripts.resize(GetScriptIdsCount(), NULL);
+
+    FillSpellSummary();
+
+    AddScripts();
+
+    // Check existance scripts for all registered by core script names
+    for (uint32 i = 1; i < GetScriptIdsCount(); ++i)
+    {
+        if (!m_scripts[i])
+            error_log("SD2: No script found for ScriptName '%s'.", GetScriptName(i));
+    }
+
+    outstring_log(">> Loaded %i C++ Scripts.", num_sc_scripts);
+}
+
+//*********************************
+//*** Functions used internally ***
+
+void Script::RegisterSelf(bool bReportError)
+{
+    if (uint32 id = GetScriptId(Name.c_str()))
+    {
+        m_scripts[id] = this;
+        ++num_sc_scripts;
+    }
+    else
+    {
+        if (bReportError)
+            error_log("SD2: Script registering but ScriptName %s is not assigned in database. Script will not be used.", Name.c_str());
+
+        delete this;
+    }
+}
+
+//********************************
+//*** Functions to be Exported ***
+
+MANGOS_DLL_EXPORT
+char const* GetScriptLibraryVersion()
+{
+    return "MaNGOS Scripting Library.";
+}
+
+MANGOS_DLL_EXPORT
+bool GossipHello(Player* pPlayer, Creature* pCreature)
+{
+    Script* pTempScript = m_scripts[pCreature->GetScriptId()];
+
+    if (!pTempScript || !pTempScript->pGossipHello)
+        return false;
+
+    pPlayer->PlayerTalkClass->ClearMenus();
+
+    return pTempScript->pGossipHello(pPlayer, pCreature);
+}
+
+MANGOS_DLL_EXPORT
+bool GOGossipHello(Player* pPlayer, GameObject* pGo)
+{
+    Script* pTempScript = m_scripts[pGo->GetGOInfo()->ScriptId];
+
+    if (!pTempScript || !pTempScript->pGossipHelloGO)
+        return false;
+
+    pPlayer->PlayerTalkClass->ClearMenus();
+
+    return pTempScript->pGossipHelloGO(pPlayer, pGo);
+}
+
+MANGOS_DLL_EXPORT
+bool GossipSelect(Player* pPlayer, Creature* pCreature, uint32 uiSender, uint32 uiAction)
+{
+    debug_log("SD2: Gossip selection, sender: %u, action: %u", uiSender, uiAction);
+
+    Script* pTempScript = m_scripts[pCreature->GetScriptId()];
+
+    if (!pTempScript || !pTempScript->pGossipSelect)
+        return false;
+
+    pPlayer->PlayerTalkClass->ClearMenus();
+
+    return pTempScript->pGossipSelect(pPlayer, pCreature, uiSender, uiAction);
+}
+
+MANGOS_DLL_EXPORT
+bool GOGossipSelect(Player* pPlayer, GameObject* pGo, uint32 uiSender, uint32 uiAction)
+{
+    debug_log("SD2: GO Gossip selection, sender: %u, action: %u", uiSender, uiAction);
+
+    Script* pTempScript = m_scripts[pGo->GetGOInfo()->ScriptId];
+
+    if (!pTempScript || !pTempScript->pGossipSelectGO)
+        return false;
+
+    pPlayer->PlayerTalkClass->ClearMenus();
+
+    return pTempScript->pGossipSelectGO(pPlayer, pGo, uiSender, uiAction);
+}
+
+MANGOS_DLL_EXPORT
+bool GossipSelectWithCode(Player* pPlayer, Creature* pCreature, uint32 uiSender, uint32 uiAction, const char* sCode)
+{
+    debug_log("SD2: Gossip selection with code, sender: %u, action: %u", uiSender, uiAction);
+
+    Script* pTempScript = m_scripts[pCreature->GetScriptId()];
+
+    if (!pTempScript || !pTempScript->pGossipSelectWithCode)
+        return false;
+
+    pPlayer->PlayerTalkClass->ClearMenus();
+
+    return pTempScript->pGossipSelectWithCode(pPlayer, pCreature, uiSender, uiAction, sCode);
+}
+
+MANGOS_DLL_EXPORT
+bool GOGossipSelectWithCode(Player* pPlayer, GameObject* pGo, uint32 uiSender, uint32 uiAction, const char* sCode)
+{
+    debug_log("SD2: GO Gossip selection with code, sender: %u, action: %u", uiSender, uiAction);
+
+    Script* pTempScript = m_scripts[pGo->GetGOInfo()->ScriptId];
+
+    if (!pTempScript || !pTempScript->pGossipSelectGOWithCode)
+        return false;
+
+    pPlayer->PlayerTalkClass->ClearMenus();
+
+    return pTempScript->pGossipSelectGOWithCode(pPlayer, pGo, uiSender, uiAction, sCode);
+}
+
+MANGOS_DLL_EXPORT
+bool QuestAccept(Player* pPlayer, Creature* pCreature, const Quest* pQuest)
+{
+    Script* pTempScript = m_scripts[pCreature->GetScriptId()];
+
+    if (!pTempScript || !pTempScript->pQuestAcceptNPC)
+        return false;
+
+    pPlayer->PlayerTalkClass->ClearMenus();
+
+    return pTempScript->pQuestAcceptNPC(pPlayer, pCreature, pQuest);
+}
+
+MANGOS_DLL_EXPORT
+bool QuestRewarded(Player* pPlayer, Creature* pCreature, Quest const* pQuest)
+{
+    Script* pTempScript = m_scripts[pCreature->GetScriptId()];
+
+    if (!pTempScript || !pTempScript->pQuestRewardedNPC)
+        return false;
+
+    pPlayer->PlayerTalkClass->ClearMenus();
+
+    return pTempScript->pQuestRewardedNPC(pPlayer, pCreature, pQuest);
+}
+
+MANGOS_DLL_EXPORT
+uint32 GetNPCDialogStatus(Player* pPlayer, Creature* pCreature)
+{
+    Script* pTempScript = m_scripts[pCreature->GetScriptId()];
+
+    if (!pTempScript || !pTempScript->pDialogStatusNPC)
+        return 100;
+
+    pPlayer->PlayerTalkClass->ClearMenus();
+
+    return pTempScript->pDialogStatusNPC(pPlayer, pCreature);
+}
+
+MANGOS_DLL_EXPORT
+uint32 GetGODialogStatus(Player* pPlayer, GameObject* pGo)
+{
+    Script* pTempScript = m_scripts[pGo->GetGOInfo()->ScriptId];
+
+    if (!pTempScript || !pTempScript->pDialogStatusGO)
+        return 100;
+
+    pPlayer->PlayerTalkClass->ClearMenus();
+
+    return pTempScript->pDialogStatusGO(pPlayer, pGo);
+}
+
+MANGOS_DLL_EXPORT
+bool ItemQuestAccept(Player* pPlayer, Item* pItem, Quest const* pQuest)
+{
+    Script* pTempScript = m_scripts[pItem->GetProto()->ScriptId];
+
+    if (!pTempScript || !pTempScript->pQuestAcceptItem)
+        return false;
+
+    pPlayer->PlayerTalkClass->ClearMenus();
+
+    return pTempScript->pQuestAcceptItem(pPlayer, pItem, pQuest);
+}
+
+MANGOS_DLL_EXPORT
+bool GOUse(Player* pPlayer, GameObject* pGo)
+{
+    Script* pTempScript = m_scripts[pGo->GetGOInfo()->ScriptId];
+
+    if (!pTempScript || !pTempScript->pGOUse)
+        return false;
+
+    return pTempScript->pGOUse(pPlayer, pGo);
+}
+
+MANGOS_DLL_EXPORT
+bool GOQuestAccept(Player* pPlayer, GameObject* pGo, const Quest* pQuest)
+{
+    Script* pTempScript = m_scripts[pGo->GetGOInfo()->ScriptId];
+
+    if (!pTempScript || !pTempScript->pQuestAcceptGO)
+        return false;
+
+    pPlayer->PlayerTalkClass->ClearMenus();
+
+    return pTempScript->pQuestAcceptGO(pPlayer, pGo, pQuest);
+}
+
+MANGOS_DLL_EXPORT
+bool GOQuestRewarded(Player* pPlayer, GameObject* pGo, Quest const* pQuest)
+{
+    Script* pTempScript = m_scripts[pGo->GetGOInfo()->ScriptId];
+
+    if (!pTempScript || !pTempScript->pQuestRewardedGO)
+        return false;
+
+    pPlayer->PlayerTalkClass->ClearMenus();
+
+    return pTempScript->pQuestRewardedGO(pPlayer, pGo, pQuest);
+}
+
+MANGOS_DLL_EXPORT
+bool AreaTrigger(Player* pPlayer, AreaTriggerEntry const* atEntry)
+{
+    Script* pTempScript = m_scripts[GetAreaTriggerScriptId(atEntry->id)];
+
+    if (!pTempScript || !pTempScript->pAreaTrigger)
+        return false;
+
+    return pTempScript->pAreaTrigger(pPlayer, atEntry);
+}
+
+MANGOS_DLL_EXPORT
+bool ProcessEvent(uint32 uiEventId, Object* pSource, Object* pTarget, bool bIsStart)
+{
+    Script* pTempScript = m_scripts[GetEventIdScriptId(uiEventId)];
+
+    if (!pTempScript || !pTempScript->pProcessEventId)
+        return false;
+
+    // bIsStart may be false, when event is from taxi node events (arrival=false, departure=true)
+    return pTempScript->pProcessEventId(uiEventId, pSource, pTarget, bIsStart);
+}
+
+MANGOS_DLL_EXPORT
+CreatureAI* GetCreatureAI(Creature* pCreature)
+{
+    Script* pTempScript = m_scripts[pCreature->GetScriptId()];
+
+    if (!pTempScript || !pTempScript->GetAI)
+        return NULL;
+
+    return pTempScript->GetAI(pCreature);
+}
+
+MANGOS_DLL_EXPORT
+bool ItemUse(Player* pPlayer, Item* pItem, SpellCastTargets const& targets)
+{
+    Script* pTempScript = m_scripts[pItem->GetProto()->ScriptId];
+
+    if (!pTempScript || !pTempScript->pItemUse)
+        return false;
+
+    return pTempScript->pItemUse(pPlayer, pItem, targets);
+}
+
+MANGOS_DLL_EXPORT
+bool EffectDummyCreature(Unit* pCaster, uint32 spellId, SpellEffectIndex effIndex, Creature* pTarget)
+{
+    Script* pTempScript = m_scripts[pTarget->GetScriptId()];
+
+    if (!pTempScript || !pTempScript->pEffectDummyNPC)
+        return false;
+
+    return pTempScript->pEffectDummyNPC(pCaster, spellId, effIndex, pTarget);
+}
+
+MANGOS_DLL_EXPORT
+bool EffectDummyGameObject(Unit* pCaster, uint32 spellId, SpellEffectIndex effIndex, GameObject* pTarget)
+{
+    Script* pTempScript = m_scripts[pTarget->GetGOInfo()->ScriptId];
+
+    if (!pTempScript || !pTempScript->pEffectDummyGO)
+        return false;
+
+    return pTempScript->pEffectDummyGO(pCaster, spellId, effIndex, pTarget);
+}
+
+MANGOS_DLL_EXPORT
+bool EffectDummyItem(Unit* pCaster, uint32 spellId, SpellEffectIndex effIndex, Item* pTarget)
+{
+    Script* pTempScript = m_scripts[pTarget->GetProto()->ScriptId];
+
+    if (!pTempScript || !pTempScript->pEffectDummyItem)
+        return false;
+
+    return pTempScript->pEffectDummyItem(pCaster, spellId, effIndex, pTarget);
+}
+
+MANGOS_DLL_EXPORT
+bool AuraDummy(Aura const* pAura, bool bApply)
+{
+    Script* pTempScript = m_scripts[((Creature*)pAura->GetTarget())->GetScriptId()];
+
+    if (!pTempScript || !pTempScript->pEffectAuraDummy)
+        return false;
+
+    return pTempScript->pEffectAuraDummy(pAura, bApply);
+}
+
+MANGOS_DLL_EXPORT
+InstanceData* CreateInstanceData(Map* pMap)
+{
+    Script* pTempScript = m_scripts[pMap->GetScriptId()];
+
+    if (!pTempScript || !pTempScript->GetInstanceData)
+        return NULL;
+
+    return pTempScript->GetInstanceData(pMap);
+}
