b85b1c352c1895c55df4a7d8f3d0524fb1104a70
lovelol <tifxoxo@web.de>
2012-08-03 17:42:50 +0200

    Don't remove Auras with IsRemovedOnShapeLost() at FORM_SHADOW.
    Signed-off-by: lovelol <tifxoxo@web.de>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -5545,6 +5545,9 @@ void Aura::HandleShapeshiftBoosts(bool apply)
         {
             if (itr->second->IsRemovedOnShapeLost())
             {
+                if (form == FORM_SHADOW)
+                    return;
+
                 target->RemoveAurasDueToSpell(itr->second->GetId());
                 itr = tAuras.begin();
             }
