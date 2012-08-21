37f786fb4390a2081bad6f391150f0208d0abb2c
Carl Hjerpe <lillecarl@gmail.com>
2012-08-21 05:32:29 +0200

    This is for testing purpose, cannot find enough people to test with 10 wow clients, 5 will have to do for now.

--- src/game/BattleGroundMgr.cpp
+++ src/game/BattleGroundMgr.cpp
@@ -677,9 +677,12 @@ bool BattleGroundQueue::CheckNormalMatch(BattleGround* bg_template, BattleGround
         return true;
 
     // If there are enough players in both pools combined and divided by 2 return true
-    if (sWorld.getConfig(CONFIG_BOOL_BATTLEGROUND_CROSSFACTION_ENABLED) 
-        && (m_SelectionPools[BG_TEAM_ALLIANCE].GetPlayerCount() + m_SelectionPools[BG_TEAM_HORDE].GetPlayerCount()) / 2 >= minPlayers)
-        return true;
+    if (sWorld.getConfig(CONFIG_BOOL_BATTLEGROUND_CROSSFACTION_ENABLED && bg_template->isBattleGround()) 
+    {
+        uint32 TotalQueuers = m_SelectionPools[BG_TEAM_ALLIANCE].GetPlayerCount() + m_SelectionPools[BG_TEAM_HORDE].GetPlayerCount();
+        if (TotalQueuers >= minPlayers)
+            return true;
+    }
 
     //return true if there are enough players in selection pools - enable to work .debug bg command correctly
     return m_SelectionPools[BG_TEAM_ALLIANCE].GetPlayerCount() >= minPlayers && m_SelectionPools[BG_TEAM_HORDE].GetPlayerCount() >= minPlayers;
